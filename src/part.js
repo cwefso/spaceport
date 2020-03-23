1841f8e593fd505f8e9a42254517a9b7de624eba
Charles Wefso <cwefso@gmail.com>
2020-03-22 21:26:20 -0600

    Refactors src/part.js

--- src/part.js
+++ src/part.js
@@ -1,26 +1,31 @@
 var validTypes = [
-      'shell',
-      'hyperdrive',
-      'computer',
-      'life support',
-      'landing gear',
-      undefined];
+    'shell',
+    'hyperdrive',
+    'computer',
+    'life support',
+    'landing gear',
+    undefined];
 
 class Part {
   constructor(item) {
     this.name = item.name;
-    this.type = item.type === validTypes[0] || item.type === validTypes[1] || item.type === validTypes [2] || item.type === validTypes [3] || item.type === validTypes[4] ? item.type : undefined;
+    this.type = item.type === validTypes[0] ||
+    item.type === validTypes [1] ||
+    item.type === validTypes [2] ||
+    item.type === validTypes [3] ||
+    item.type === validTypes [4] ? item.type : undefined;
     this.value = item.value;
     this.broken = false;
   }
 
   isValid() {
-     if (this.name!= undefined &&
-         this.type != undefined &&
-         this.value != undefined)
-         { return true; } else { return false; }
-            };
-
-  }
+    if (this.name != undefined &&
+      this.type != undefined &&
+      this.value != undefined)
+      { return true;
+    } else {
+      return false; }
+  };
+};
 
 module.exports = Part;
